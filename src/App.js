19e4846673bcea9c299cb326b5b73b2054f39d08
Carlos Ambeliz <carlosambeliz@hotmail.com>
2023-09-22 13:03:34 -0600

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        Estoy probando
       </header>
       <div id="main">
         <p id="hora"></p>
-        <div class="rebote">
-          <div id="btn-login" ><span>Ingresar</span> <span class="material-symbols-outlined">
+        <div className="rebote">
+          <div id="btn-login" ><span>Ingresar</span> <span className="material-symbols-outlined">
             login
           </span></div>
         </div>
